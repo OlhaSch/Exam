067b47968d293c2ebbd40823fa52415da7769a3f
OlhaSch <163459477+OlhaSch@users.noreply.github.com>
2024-05-17 02:35:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 from flask import Flask, render_template
-=======
+
 from flask import Flask
->>>>>>> 2f5c70a006a5e683ea996b0b8dc35e0b0f98c2e2
+
 
 app = Flask(__name__)
 
@@ -10,14 +10,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-<<<<<<< HEAD
+
 @app.route('/admin_panel')
 def admin_panel():
     return render_template('admin_panel.html')
 
 if __name__ == "__main__":
     app.run()
-=======
+
 if __name__ == "__main__":
     app.run()
->>>>>>> 2f5c70a006a5e683ea996b0b8dc35e0b0f98c2e2
+
