f83b6492d5ccd37f382ca91c51f1484a42c5249e
olgasherbiena <60242066+olgasherbiena@users.noreply.github.com>
2024-05-17 18:57:46 +0300

    2

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
 @app.route('/admin_panel')
 def admin_panel():
     return render_template('admin_panel.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
 
 if __name__ == "__main__":
     app.run()
