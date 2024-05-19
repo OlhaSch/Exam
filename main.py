af4b0151edcbfd0d90e6441ae04fca35f45cef32
olgasherbiena <60242066+olgasherbiena@users.noreply.github.com>
2024-05-19 15:59:02 +0300

    update 19.05 16:00

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/admin_panel')
-def admin_panel():
-    return render_template('admin_panel.html')
+@app.route('/admin_panel_auth')
+def admin_panel_auth():
+    return render_template('admin_panel_auth.html')
 
 @app.route('/registration')
 def registration():
     return render_template('registration.html')
+
+@app.route('/admin_panel')
+def admin_panel():
+    return render_template('admin_panel.html')
 
 if __name__ == "__main__":
     app.run()
