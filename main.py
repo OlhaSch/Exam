f50a98608543fadc1d5b98c59a01d438bc78f2b7
olgasherbiena <60242066+olgasherbiena@users.noreply.github.com>
2024-05-19 18:04:58 +0300

    add test-template

--- main.py
+++ main.py
@@ -15,5 +15,9 @@ def registration():
 def admin_panel():
     return render_template('admin_panel.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 if __name__ == "__main__":
     app.run()
